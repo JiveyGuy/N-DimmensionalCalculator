9cc0949486acc17059559254db18bfc277c868a3
JiveyGuy <jason.ivey.not.micheal@gmail.com>
2017-01-27 02:26:19 -0700

    Add files via upload

---
+++ Node (1).java
@@ -0,0 +1,38 @@
+// Jason Ivey Node Class for Que Data Structure
+public class Node{
+  //private double value is the numeric value of a node 
+  private double value;
+  // Node next allows for the data structure to be recursivly accesed 
+  private Node next;
+  
+  //Constructor for the node object (double, Node)
+  public Node(double inputVal, Node inputNode){
+    //initializes object's atributtes
+    this.value = inputVal;
+    this.next = inputNode;
+  }
+  
+  //overloaded constructor for incindents of null next node (double)
+  public Node(double inputVal) {
+    this.value = inputVal;
+    //meaningless assingment operation, included only to highlight null state
+    this.next = null;
+  }
+  
+  //accessor and mutator methods ahead
+  public double getVal(){
+    return this.value;
+  }
+  
+  public Node getNext(){
+   return this.next; 
+  }
+  
+  public void setVal(double inputVal){
+    this.value = inputVal;
+  }
+  
+  public void setNext(Node inputNode){
+   this.next = inputNode; 
+  }
+}
